870d9eb6a2786ba8ffb792d916778337fe386093
Christian Jodi <bluejackers514@gmail.com>
2025-10-12 20:37:30 +0700

    fixing merging tadi karna ada conflict, update routes buat compatible sama about page

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< Updated upstream
 use App\Http\Controllers\LoginController;
 
 Route::middleware('web')->group(function () {
     Route::get('/', function () {
         return view('landing');
-    });
+    })->name('home');
 
     Route::get('/login', function () {
         return view('login');
     })->name('login');
 
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
+    
     Route::get('/register', function () {
         return view('register');
     });
@@ -26,15 +29,6 @@ Route::middleware('web')->group(function () {
         return redirect()->route('login');
     })->name('logout');
 });
-=======
-use App\Http\Controllers\RegisterController;
-
-
-Route::get('/', function () {
-    return view('landing');
-})->name('home');
-
->>>>>>> Stashed changes
 
 Route::middleware(['web', 'auth'])->group(function () {
     Route::get('/teacherDashboard', function () {
@@ -45,26 +39,3 @@ Route::middleware(['web', 'auth'])->group(function () {
         return view('studentDashboard');
     });
 });
-<<<<<<< Updated upstream
-=======
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-
-Route::get('/teacherDashboard', function () {
-    return view('teacherDashboard');
-});
-
-Route::get('/studentDashboard', function () {
-    return view('studentDashboard');
-});
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::post('/postlogin', 'LoginController@postLogin')->name('postlogin');
-
->>>>>>> Stashed changes
