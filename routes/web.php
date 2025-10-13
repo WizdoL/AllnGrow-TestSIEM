b71fa8359607a983921d2d6dcbb37d0eb0cb935b
Christian <christiantjandra33@gmail.com>
2025-10-13 19:46:40 +0700

    add RegisterController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::middleware('web')->group(function () {
     Route::get('/', function () {
